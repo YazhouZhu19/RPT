b7505f7747f3b0555adb10f9ff2929a85553a28d
Yazhou Zhu <132653569+YazhouZhu19@users.noreply.github.com>
2023-07-03 16:17:22 +0800

    Add files via upload

---
+++ scripts/train.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+GPUID1=0
+export CUDA_VISIBLE_DEVICES=$GPUID1
+
+###### Shared configs ######
+DATASET='CHAOST2'
+NWORKER=16
+RUNS=1
+ALL_EV=(0 1 2 3 4) # 5-fold cross validation (0, 1, 2, 3, 4)
+TEST_LABEL=[1,2,3,4]
+EXCLUDE_LABEL=None
+TEST=1234  # flag for folder name, '1234' for not excluding any labels,  '14' for excluding label [2,3], '23' for excluding label [1,4]
+USE_GT=False
+###### Training configs ######
+NSTEP=40000
+DECAY=0.98
+
+MAX_ITER=5000 # defines the size of an epoch
+SNAPSHOT_INTERVAL=5000 # interval for saving snapshot
+SEED=2023
+
+echo ========================================================================
+
+for EVAL_FOLD in "${ALL_EV[@]}"
+do
+  PREFIX="train_${DATASET}_cv${EVAL_FOLD}"
+  echo $PREFIX
+  LOGDIR="./exps_on_${DATASET}_fewshot"
+
+  if [ ! -d $LOGDIR ]
+  then
+    mkdir -p $LOGDIR
+  fi
+
+  python3 train.py with \
+  mode='train' \
+  dataset=$DATASET \
+  num_workers=$NWORKER \
+  n_steps=$NSTEP \
+  eval_fold=$EVAL_FOLD \
+  test_label=$TEST_LABEL \
+  exclude_label=$EXCLUDE_LABEL \
+  use_gt=$USE_GT \
+  max_iters_per_load=$MAX_ITER \
+  seed=$SEED \
+  save_snapshot_every=$SNAPSHOT_INTERVAL \
+  lr_step_gamma=$DECAY \
+  path.log_dir=$LOGDIR
+done
