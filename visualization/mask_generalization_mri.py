5f198d2136c4f80a1b9e721e6ad339cb5d00936e
Yazhou Zhu <132653569+YazhouZhu19@users.noreply.github.com>
2024-06-08 13:59:23 +0800

    Create mask_generalization_mri.py

---
+++ visualization/mask_generalization_mri.py
@@ -0,0 +1,115 @@
+# the code for generating masks for ABD-MRI
+
+import os
+import cv2
+import numpy as np
+import SimpleITK as itk
+import matplotlib.pyplot as plt
+import matplotlib
+import cv2
+import SimpleITK as itk
+
+# Abd_MRI dataset
+
+
+abd_ct_pth = './data/Abd_CT/'
+abd_mri_pth = './data/Abd_MRI/'
+cmr_pth = './data/CMR/'
+
+abd_mri_gt_pth = os.path.join(abd_mri_pth, 'Abd_MRI_x_GT.nii.gz')
+abd_mri_img_pth = os.path.join(abd_mri_pth, 'Abd_MRI_x.nii.gz')
+
+abd_mri_liver_pth = os.path.join(abd_mri_pth, 'prediction_x_LIVER.nii.gz')
+abd_mri_spleen_pth = os.path.join(abd_mri_pth, 'prediction_x_SPLEEN.nii.gz')
+abd_mri_rk_pth = os.path.join(abd_mri_pth, 'prediction_x_RK.nii.gz')
+abd_mri_lk_pth = os.path.join(abd_mri_pth, 'prediction_x_LK.nii.gz')
+
+abd_mri_gt = itk.GetArrayFromImage(itk.ReadImage(abd_mri_gt_pth))  
+abd_mri_img = itk.GetArrayFromImage(itk.ReadImage(abd_mri_img_pth))  
+
+
+abd_mri_gt[abd_mri_gt == 200] = 1
+abd_mri_gt[abd_mri_gt == 500] = 2
+abd_mri_gt[abd_mri_gt == 600] = 3
+
+# ********************************liver**********************************
+abd_mri_liver = itk.GetArrayFromImage(itk.ReadImage(abd_mri_liver_pth))
+abd_mri_liver_gt = 1 * (abd_mri_gt == 1)
+idx = abd_mri_liver_gt.sum(axis=(1, 2)) > 0
+abd_mri_liver_gt = abd_mri_liver_gt[idx]
+abd_mri_img_liver = abd_mri_img[idx]
+
+abd_mri_liver_gt_show = abd_mri_liver_gt[11] * 200
+abd_mri_img_show = abd_mri_img_liver[11] / 4.5
+abd_mri_liver_show = abd_mri_liver[11] * 200
+
+abd_mri_liver_spt = abd_mri_liver_gt[5] * 200
+abd_mri_img_spt = abd_mri_img_liver[5] / 4.5
+
+cv2.imwrite("./data/Abd_MRI/abd_mri_liver_gt.png", abd_mri_liver_gt_show)
+cv2.imwrite("./data/Abd_MRI/abd_mri_liver_img.png", abd_mri_img_show)
+cv2.imwrite("./data/Abd_MRI/abd_mri_liver.png", abd_mri_liver_show)
+
+cv2.imwrite("./data/Abd_MRI/abd_mri_liver_spt.png", abd_mri_liver_spt)
+cv2.imwrite("./data/Abd_MRI/abd_mri_liver_img_spt.png", abd_mri_img_spt)
+
+# **********************************spleen*********************************
+abd_mri_spleen = itk.GetArrayFromImage(itk.ReadImage(abd_mri_spleen_pth))
+abd_mri_spleen_gt = 1 * (abd_mri_gt == 4)
+idx = abd_mri_spleen_gt.sum(axis=(1, 2)) > 0
+abd_mri_spleen_gt = abd_mri_spleen_gt[idx]
+abd_mri_img_spleen = abd_mri_img[idx]
+
+abd_mri_spleen_gt_show = abd_mri_spleen_gt[10]*200
+abd_mri_img_show = abd_mri_img_spleen[10] / 4.5
+
+abd_mri_spleen_spt = abd_mri_spleen_gt[4]*200
+abd_mri_img_spt = abd_mri_img_spleen[4] / 4.5
+
+cv2.imwrite("./data/Abd_MRI/abd_mri_spleen_gt.png", abd_mri_spleen_gt_show)
+cv2.imwrite("./data/Abd_MRI/abd_mri_spleen_img.png", abd_mri_img_show)
+cv2.imwrite("./data/Abd_MRI/abd_mri_spleen.png", abd_mri_spleen_show)
+
+cv2.imwrite("./data/Abd_MRI/abd_mri_spleen_spt.png", abd_mri_spleen_spt)
+cv2.imwrite("./data/Abd_MRI/abd_mri_spleen_img_spt.png", abd_mri_img_spt)
+
+# **********************************RK************************************
+abd_mri_rk = itk.GetArrayFromImage(itk.ReadImage(abd_mri_rk_pth))
+abd_mri_rk_gt = 1 * (abd_mri_gt == 2)
+idx = abd_mri_rk_gt.sum(axis=(1, 2)) > 0
+abd_mri_rk_gt = abd_mri_rk_gt[idx]
+abd_mri_img_rk = abd_mri_img[idx]
+
+abd_mri_rk_gt_show = abd_mri_rk_gt[10]*200
+abd_mri_img_show = abd_mri_img_rk[10] / 4.5
+
+abd_mri_rk_spt = abd_mri_rk_gt[11]*200
+abd_mri_img_spt = abd_mri_img_rk[11] / 4.5
+
+cv2.imwrite("./data/Abd_MRI/abd_mri_rk_gt.png", abd_mri_rk_gt_show)
+cv2.imwrite("./data/Abd_MRI/abd_mri_rk_img.png", abd_mri_img_show)
+cv2.imwrite("./data/Abd_MRI/abd_mri_rk.png", abd_mri_rk_show)
+
+cv2.imwrite("./data/Abd_MRI/abd_mri_rk_spt.png", abd_mri_rk_spt)
+cv2.imwrite("./data/Abd_MRI/abd_mri_rk_img_spt.png", abd_mri_img_spt)
+
+# *********************************LK**************************************
+abd_mri_lk = itk.GetArrayFromImage(itk.ReadImage(abd_mri_lk_pth))
+abd_mri_lk_gt = 1 * (abd_mri_gt == 3)
+idx = abd_mri_lk_gt.sum(axis=(1, 2)) > 0
+abd_mri_lk_gt = abd_mri_lk_gt[idx]
+abd_mri_img_lk = abd_mri_img[idx]
+
+abd_mri_lk_gt_show = abd_mri_lk_gt[11]*200
+abd_mri_img_show = abd_mri_img_lk[11] / 4.5
+
+abd_mri_lk_spt = abd_mri_lk_gt[5]*200
+abd_mri_img_spt = abd_mri_img_lk[5] / 4.5
+
+cv2.imwrite("./data/Abd_MRI/abd_mri_lk_gt.png", abd_mri_lk_gt_show)
+cv2.imwrite("./data/Abd_MRI/abd_mri_lk_img.png", abd_mri_img_show)
+cv2.imwrite("./data/Abd_MRI/abd_mri_lk.png", abd_mri_lk_show)
+
+cv2.imwrite("./data/Abd_MRI/abd_mri_lk_spt.png", abd_mri_lk_spt)
+cv2.imwrite("./data/Abd_MRI/abd_mri_lk_img_spt.png", abd_mri_img_spt)
+
