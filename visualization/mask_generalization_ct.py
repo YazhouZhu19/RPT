6e5dcbaa6bf959a7963f3dae3d694fce2082bfd6
Yazhou Zhu <132653569+YazhouZhu19@users.noreply.github.com>
2024-06-09 22:44:18 +0800

    Update mask_generalization_ct.py

--- visualization/mask_generalization_ct.py
+++ visualization/mask_generalization_ct.py
@@ -38,14 +38,14 @@ abd_ct_liver_gt = abd_ct_liver_gt[idx]
 abd_ct_img_liver = abd_ct_img[idx]
 
 abd_ct_liver_gt_show = abd_ct_liver_gt[16] * 200  # choose the 16th slice of case x to illustrate, you also can choose other slices
-abd_ct_img_show = abd_ct_img_liver[16]
+abd_ct_img_show_1 = abd_ct_img_liver[16]
 abd_ct_liver_show = abd_ct_liver[16] * 200
 
 abd_ct_liver_spt = abd_ct_liver_gt[13] * 200   # choose the 13th slice of case x as support image.  
 abd_ct_img_spt = abd_ct_img_liver[13]
 
 cv2.imwrite("./data/Abd_CT/abd_ct_liver_gt.png", abd_ct_liver_gt_show)    # ground truth 
-cv2.imwrite("./data/Abd_CT/abd_ct_liver_img.png", abd_ct_img_show)        # the image 
+cv2.imwrite("./data/Abd_CT/abd_ct_liver_img.png", abd_ct_img_show_1)        # the image 
 cv2.imwrite("./data/Abd_CT/abd_ct_liver.png", abd_ct_liver_show)          # the liver prediction  
 
 cv2.imwrite("./data/Abd_CT/abd_ct_liver_spt.png", abd_ct_liver_spt)       # the liver mask of support image
@@ -59,13 +59,13 @@ abd_ct_spleen_gt = abd_ct_spleen_gt[idx]
 abd_ct_img_spleen = abd_ct_img[idx]
 
 abd_ct_spleen_gt_show = abd_ct_spleen_gt[14]*200
-abd_ct_img_show = abd_ct_img_spleen[14]
+abd_ct_img_show_2 = abd_ct_img_spleen[14]
 
 abd_ct_spleen_spt = abd_ct_spleen_gt[8]*200
 abd_ct_img_spt = abd_ct_img_spleen[8]
 
 cv2.imwrite("./data/Abd_CT/abd_ct_spleen_gt.png", abd_ct_spleen_gt_show)
-cv2.imwrite("./data/Abd_CT/abd_ct_spleen_img.png", abd_ct_img_show)
+cv2.imwrite("./data/Abd_CT/abd_ct_spleen_img.png", abd_ct_img_show_2)
 cv2.imwrite("./data/Abd_CT/abd_ct_spleen.png", abd_ct_spleen_show)
 
 cv2.imwrite("./data/Abd_CT/abd_ct_spleen_spt.png", abd_ct_spleen_spt)
@@ -79,13 +79,13 @@ abd_ct_rk_gt = abd_ct_rk_gt[idx]
 abd_ct_img_rk = abd_ct_img[idx]
 
 abd_ct_rk_gt_show = abd_ct_rk_gt[18]*200
-abd_ct_img_show = abd_ct_img_rk[18]
+abd_ct_img_show_3 = abd_ct_img_rk[18]
 
 abd_ct_rk_spt = abd_ct_rk_gt[10]*200
 abd_ct_img_spt = abd_ct_img_rk[10]
 
 cv2.imwrite("./data/Abd_CT/abd_ct_rk_gt.png", abd_ct_rk_gt_show)
-cv2.imwrite("./data/Abd_CT/abd_ct_rk_img.png", abd_ct_img_show)
+cv2.imwrite("./data/Abd_CT/abd_ct_rk_img.png", abd_ct_img_show_3)
 cv2.imwrite("./data/Abd_CT/abd_ct_rk.png", abd_ct_rk_show)
 
 cv2.imwrite("./data/Abd_CT/abd_ct_rk_spt.png", abd_ct_rk_spt)
@@ -99,13 +99,13 @@ abd_ct_lk_gt = abd_ct_lk_gt[idx]
 abd_ct_img_lk = abd_ct_img[idx]
 
 abd_ct_lk_gt_show = abd_ct_lk_gt[17]*200
-abd_ct_img_show = abd_ct_img_lk[17]
+abd_ct_img_show_4 = abd_ct_img_lk[17]
 
 abd_ct_lk_spt = abd_ct_lk_gt[8]*200
 abd_ct_img_spt = abd_ct_img_lk[8]
 
 cv2.imwrite("./data/Abd_CT/abd_ct_lk_gt.png", abd_ct_lk_gt_show)
-cv2.imwrite("./data/Abd_CT/abd_ct_lk_img.png", abd_ct_img_show)
+cv2.imwrite("./data/Abd_CT/abd_ct_lk_img.png", abd_ct_img_show_4)
 cv2.imwrite("./data/Abd_CT/abd_ct_lk.png", abd_ct_lk_show)
 
 cv2.imwrite("./data/Abd_CT/abd_ct_lk_spt.png", abd_ct_lk_spt)
